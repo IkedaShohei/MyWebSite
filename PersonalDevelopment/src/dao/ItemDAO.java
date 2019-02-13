9adee186faab582e771a7f0442f75890a2dec6f2
ikedashohei <shohei.ikeda.likeit@gmail.com>
2019-02-13 10:26:46 +0900

    在庫数管理実装前

--- PersonalDevelopment/src/dao/ItemDAO.java
+++ PersonalDevelopment/src/dao/ItemDAO.java
@@ -41,7 +41,6 @@ public class ItemDAO {
 				String fileName = rs.getString("file_name");
 				int userId = rs.getInt("add_user_id");
 
-
 				ItemDataBeans idb = new ItemDataBeans(itemId, name, detail, price, stock, fileName, userId);
 				itemList.add(idb);
 			}
@@ -112,49 +111,7 @@ public class ItemDAO {
 		return idb;
 	}
 
-//	public ItemDataBeans selectByItemId(int itemId) {
-//
-//		Connection conn = null;
-//		ItemDataBeans idb = new ItemDataBeans();
-//
-//		try {
-//			conn = DBmanager.getConnection();
-//			String sql ="SELECT * FROM item WHERE item_id = ?";
-//
-//			PreparedStatement ps;
-//			ps = conn.prepareStatement(sql);
-//			ps.setInt(1, itemId);
-//			ResultSet rs = ps.executeQuery();
-//
-//			if(!rs.next()) {
-//				return null;
-//			}
-//
-//			int item_id = rs.getInt("item_id");
-//			String name = rs.getString("name");
-//			String detail = rs.getString("detail");
-//			int price = rs.getInt("price");
-//			int stock = rs.getInt("stock");
-//			String fileName = rs.getString("file_name");
-//			int userId = rs.getInt("add_user_id");
-//
-//			idb = new ItemDataBeans(item_id, name, detail, price, stock, fileName, userId);
-//
-//		} catch (SQLException e) {
-//			// TODO 自動生成された catch ブロック
-//			e.printStackTrace();
-//		}finally {
-//			//closeメソッドでデータベースを切断する
-//            if (conn != null) {
-//                try {
-//                    conn.close();
-//                } catch (SQLException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//		}
-//		return idb;
-//	}
+
 
 	public static void insertItem(String itemName,String itemDetail,int itemPrice,String fileName,int userId) {
 		Connection conn= null;
